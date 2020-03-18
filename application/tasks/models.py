ede0d2e2455cadd427c0fa87fdecf096234e3fdd
perander <silva.perander@helsinki.fi>
2020-03-18 12:48:38 +0200

    addition and listing of tasks

---
+++ application/tasks/models.py
@@ -0,0 +1,16 @@
+from application import db
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(144), nullable=False)
+    done = db.Column(db.Boolean, nullable=False)
+    inprogress = db.Column(db.Boolean, nullable=False)
+
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+
+    def __init__(self, name):
+        self.name = name
+        self.done = False
+        self.inprogress = False
