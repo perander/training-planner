49e11849a0451b6dd1779e807aa3f303abeadeff
perander <silva.perander@helsinki.fi>
2020-04-26 22:32:14 +0300

    :bug: caseinsensitive search for heroku

--- application/views.py
+++ application/views.py
@@ -3,7 +3,7 @@ from flask_login import current_user
 from application import app, login_required
 from application.category.models import Category
 from application.tasks.models import Task
-
+from sqlalchemy import func
 
 @app.route("/")
 @login_required
@@ -22,10 +22,10 @@ def index():
 
 @app.route("/", methods=["POST"])
 def search():
-    query = request.form.get("query")
+    query = func.lower(request.form.get("query"))
 
-    foundtasks = Task.query.filter(Task.name.contains(query)).all()
-    foundcategories = Category.query.filter(Category.name.contains(query)).all()
+    foundtasks = Task.query.filter(func.lower(Task.name).contains(query)).all()
+    foundcategories = Category.query.filter(func.lower(Category.name).contains(query)).all()
 
     if (foundtasks is None or len(foundtasks) == 0) and \
             (foundcategories is None or len(foundcategories) == 0):
