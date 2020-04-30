96682e34331753c3c6f30ba89d99e115efe70765
perander <silva.perander@helsinki.fi>
2020-04-30 17:32:02 +0300

    :lipstick: change error text

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -14,7 +14,7 @@ class RegisterForm(FlaskForm):
     username = StringField("Username", [validators.Length(min=2)])
     password = PasswordField("Password", [validators.Length(min=2),
                                           validators.equal_to('confirm_password')])
-    confirm_password = PasswordField("Repeat password")
+    confirm_password = PasswordField("Confirm password")
     admin = BooleanField("Admin")
 
     class Meta:
