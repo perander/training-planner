bf48a57fffae5eb1fb087f56a4ee2e6b90630bb1
perander <silva.perander@helsinki.fi>
2020-04-30 17:29:14 +0300

    :sparkles: functionality to update categories

--- application/category/views.py
+++ application/category/views.py
@@ -2,13 +2,18 @@ from flask import redirect, render_template, request, url_for
 from flask_login import current_user
 
 from application import app, db, login_required
-from application.category.models import Category, create, exists, delete, get_all_categories, find
+from application.category.models import Category, create, exists, exists_another, delete, get_all_categories, find, \
+    update, all_categories_ordered_by_createdate
 from application.category.forms import CategoryForm
 
 
 @app.route("/categories", methods=["GET"])
 def categories_index():
-    return render_template("categories/list.html", categories=get_all_categories())
+    page = request.args.get('page', 1, type=int)
+    pagination = all_categories_ordered_by_createdate().paginate(page, per_page=10)
+    categories = pagination.items
+
+    return render_template("categories/list.html", categories=categories, pagination=pagination)
 
 
 @app.route("/categories/<category_id>/")
@@ -36,6 +41,31 @@ def categories_create():
     create(form.name.data)
     db.session().commit()
 
+    return redirect(url_for("categories_index"))
+
+
+@app.route("/update/<category_id>", methods=["GET", "POST"])
+@login_required(role="ADMIN")
+def categories_update(category_id):
+    category = find(category_id)
+
+    if request.method == "GET":
+        return render_template("categories/updateform.html",
+                               form=CategoryForm(),
+                               category=category)
+
+    form = CategoryForm(request.form)
+
+    if form.validate():
+        if exists_another(category_id, form.name.data):
+            return render_template("categories/updateform.html",
+                                   form=CategoryForm(),
+                                   category=category,
+                                   error="Category " + form.name.data + " already exists")
+
+        update(category_id, form.name.data)
+
+    db.session().commit()
     return redirect(url_for("categories_index"))
 
 
